438329c32142e580d28cca7af89fca128da38611
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-10 16:20:12 -0700

    Fix warning message about useMongoClient option

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', {
+  useMongoClient: true,
+});
 
 var db = mongoose.connection;
 
