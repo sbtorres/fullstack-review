424e83d3ff79c800fd3a2ff6ef1b83347888a391
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-10 20:39:49 -0700

    Update styling for repo's and search bar

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const RepoList = (props) => (
   <div>
-    <h4> Top 25 Repos by Stars </h4>
+    <h3> Top 25 Repos by Stars </h3>
     {props.repos.map((repo, index) => {
       return(
-        <div className="repo">
-          <div>Repository: {repo.repo_name}</div>
-          <div>Description: {repo.description}</div>
-          <div>Creator: {repo.username}</div>
-          <div>Stars: {repo.stars}</div>
-          <div>Forks: {repo.forks}</div>
-          <div>Creation Date: {repo.created_at}</div>
-          <div>Language: {repo.language}</div>
+        <div key={repo.id} className="repo">
+          <div className="name">Repository: {repo.repo_name}</div>
+          <div className="description">Description: {repo.description}</div>
+          <div className="sub-content">
+            <div className="creator">Creator: {repo.username}</div>
+            <div className="stars">Stars: {repo.stars}</div>
+            <div className="forks">Forks: {repo.forks}</div>
+            <div className="updated_at">Last Updated: {repo.updated_at}</div>
+            <div className="language">Language: {repo.language}</div>
+          </div>
         </div>
       )
     })}
